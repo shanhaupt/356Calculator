e203281c2af5bfc81454ec28fe1582d28ee1b804
Sonali Pai <sonalipa@usc.edu>
2018-05-25 17:58:41 -0700

    fixed indentation lol

--- Classes/Binary.java
+++ Classes/Binary.java
@@ -1,20 +1,20 @@
 
 public class Binary {
-	
+
 	//convert binary to decimal
 	public long binaryToDecimal(String binaryStr){
 		long decimalVal = 0;
 		int strLength = binaryStr.length();
-		
+
 		/*loop through the binary string and add the corresponding
 		decimal values to decimalVal*/
 		for (int i = 0; i < binaryStr.length(); i++) {
-	    	char c = binaryStr.charAt(i);
-	    	int currDigit = Character.getNumericValue(c);
-	    	decimalVal += (Math.pow(2, strLength-i-1)) * currDigit;
+			char c = binaryStr.charAt(i);
+			int currDigit = Character.getNumericValue(c);
+			decimalVal += (Math.pow(2, strLength-i-1)) * currDigit;
 		}
-		
+
 		return decimalVal;
 	}
-		
+
 }
--- Classes/Decimal.java
+++ Classes/Decimal.java
@@ -1,10 +1,10 @@
 
 public class Decimal {
-	
+
 	//convert decimal to binary
 	public String decimalToBinary(long decimalVal) {
 		String binaryStr = "";
-		
+
 		/*divide the decimal value by 2, append the remainder to the binary string
 		as long as the decimal value is greater than 0 */
 		while(decimalVal > 0) {
@@ -12,13 +12,13 @@ public class Decimal {
 			binaryStr += remainder;
 			decimalVal = decimalVal/2;
 		}
-		
+
 		//use a string builder to reverse the string (change if inefficient)
 		StringBuilder sb = new StringBuilder();
-        sb.append(binaryStr);
-        sb = sb.reverse();
-        binaryStr = sb.toString();
-        
+		sb.append(binaryStr);
+		sb = sb.reverse();
+		binaryStr = sb.toString();
+
 		return binaryStr;
 	}
 }
